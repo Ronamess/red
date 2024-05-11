42c9ad4b1d695fe7c1d4e13187376d86356e6914
Ronamess <157751431+Ronamess@users.noreply.github.com>
2024-05-11 23:04:19 +0330

    sample-inner-page.html

--- php/Practice 2/Reza/view/blog/new-blog.php
+++
@@ -1,9 +0,0 @@
-Of course! Designing a website is an engaging and challenging process that requires attention to technical details and aesthetic appeal. This process involves stages such as requirements analysis, user interface (UI) and user experience (UX) design, website development, testing, and optimization.
-
-Every website should cater to its specific needs and goals. To start, it's important to carefully determine the objectives, target audience, content, and technical requirements of your website.
-
-Additionally, UI and UX design are crucial. UI pertains to the visual and aesthetic design of the website, while UX focuses on the ease and efficiency of user interactions with the site.
-
-Finally, website development requires strong technical knowledge to build your site using languages and frameworks such as HTML, CSS, JavaScript, PHP, and more.
-
-It's best to rely on reputable resources and the latest methods and standards for your website design, and also regularly update your website to keep up with ongoing technological advancements and competitive trends.
