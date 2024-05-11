8fbf8c2e783d9ef814951e4f37bba97ac4af1a83
Ronamess <157751431+Ronamess@users.noreply.github.com>
2024-05-11 23:37:58 +0330

    Add files via upload

---
+++ Php/Practice 1/about.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+    <title>about we</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
+<body dir="rtl">
+    <header>
+        <?php
+        include ('view/navbar.php');
+        ?>
+    </header>
+    <div class="container text-right" style="directon: rtl !important">
+        <div class="border shadow-lg bg-success mt-3 p-3 rounded-lg">
+            <h1 class="text-center">vablog</h1>
+        </div>
+        <div class="row mt-3">
+            <div class="col-md-4 mb-3">
+                <div class="border bg-primary">
+                    <div class="text-center text-white">
+                        <h2 class="display-5 font-wight-bold">title</h2>
+                    </div>
+                    <div class="text-white p-3 text-justify">
+                    he meeting of two personalities is like the contact of two chemical substances. If there is any reaction, both are transformed.” 
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="border bg-primary">
+                    <div class="text-center text-white">
+                        <h2 class="display-5 font-wight-bold">title</h2>
+                    </div>
+                    <div class="text-white p-3 text-justify">
+                    he meeting of two personalities is like the contact of two chemical substances. If there is any reaction, both are transformed.”
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="border bg-primary">
+                    <div class="text-center text-white">
+                        <h2 class="display-5 font-wight-bold">عنوان</h2>
+                    </div>
+                    <div class="text-white p-3 text-justify">
+                    he meeting of two personalities is like the contact of two chemical substances. If there is any reaction, both are transformed.”
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>s
+</body>
---
+++ Php/Practice 1/view/navbar.php
@@ -0,0 +1,18 @@
+<div class="navbar">
+    <div class="container">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="view/blog/blog.php">vablog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about.php">about we</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="view/blog/new-blog.php">vablog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">call whit we</a>
+            </li>
+        </ul>
+    </div>
+</div> 
